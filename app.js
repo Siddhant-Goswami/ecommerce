33c97af43d8cde21549b17117361e6a0fc35427c
Siddhant Goswami <goswami.siddh@gmail.com>
2019-08-29 12:07:38 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/users");
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("DB Connected"));
+
+// routes
+app.use(userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
