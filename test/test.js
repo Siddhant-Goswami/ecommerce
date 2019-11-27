13e68040fe3866aa7604d720424b68db1e04eda9
siddhant <siddhant@tapchief.com>
2019-11-27 19:20:09 +0530

    Init mocha test for api/products

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const supertest = require("supertest");
+const assert = require('assert');
+const app = require("../app");
+
+describe("GET /api/products", function() {
+  it("it should has status code 200", function(done) {
+    supertest(app)
+      .get("/api/products")
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end(function(err, res){
+        if (err) done(err);
+        done();
+      });
+  });
+});
