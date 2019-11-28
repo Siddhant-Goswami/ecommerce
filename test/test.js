e4836d19068157d6dd6ca0325e541c27d224907b
siddhant <siddhant@tapchief.com>
2019-11-28 17:18:34 +0530

    Replaced mocha with jest

--- test/test.js
+++ test/test.js
@@ -1,15 +1,10 @@
 const supertest = require("supertest");
-const assert = require('assert');
 const app = require("../app");
 
 describe("GET /api/products", function() {
-  it("it should has status code 200", function(done) {
-    supertest(app)
-      .get("/api/products")
-      .expect(200, done)
-    //   .end(function(err, res){
-    //     if (err) done(err);
-    //     done();
-    //   });
-  });
+  it("it should has status code 200", async () => {
+    const res = await supertest(app)
+    .get('/api/products')
+  expect(res.statusCode).toEqual(200)
+})
 });
